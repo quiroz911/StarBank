6552e96e047ea7a8623a22637cbb7f3f96a748bc
Juan Pablo Quiroz <jpablo.quiroz@udea.edu.co>
2020-05-15 15:46:00 -0500

    primeros pasos controlador

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+
+    //boton para abrir la ventana menu cajero
+    public void abrirMenuCajero_action(ActionEvent actionEvent) throws IOException {
+
+            Parent root = FXMLLoader.load(getClass().getResource("/vista/menuCajero.fxml"));
+
+            Stage primaryStage = new Stage();
+
+            primaryStage.setTitle("Menu Cajero");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+
+    }
 }
